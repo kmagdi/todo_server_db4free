973dbda5db47a88d712c866b1db3ae70bd5c441e
KMagdi <46417302+kmagdi@users.noreply.github.com>
2023-04-14 16:34:50 +0200

    configDB

--- configDB.js
+++ configDB.js
@@ -2,22 +2,17 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 export const configDB={
-        host     : process.env.MYSQL_HOST || 'localhost',
-        user     : process.env.MYSQL_USERNAME || 'root',
-        password : process.env.MYSQL_PASSWORD || '',
-        database : process.env.MYSQL_DATABASE || 'books'
+        host     :  process.env.MYSQL_HOST,
+        user     : process.env.MYSQL_USERNAME,
+        password :process.env.MYSQL_PASSWORD,
+        database : process.env.MYSQL_DATABASE,
+        port:3306
 }
-//MYSQL_USERNAME="czf3fhh3dirhct1d"
-/*console.log('process.env.MYSQL_HOST:',process.env.MYSQL_HOST);
-console.log('process.env.MYSQL_USERNAME:',process.env.USERNAME,process.env.MYSQL_PASSWORD);
-console.log('process.env.MYSQL_DATABASE:',process.env.MYSQL_DATABASE);
-console.log('process.env.PORT:',process.env.PORT);*/
-//console.log("Database_URL", process.env.DATABASE_URL);
-
-/*export const configDB={
-        host     :'localhost',
-        user     : 'root',
-        password : '',
-        database:'todo'
-}*/
+export const configDB2={
+        host     : 'localhost' ,
+        user     : 'root' ,
+        password :'' ,
+        database : 'todo' ,
+        port:3306
+}
 console.log('a használt adatbázis szolgáltató:',configDB.host)
